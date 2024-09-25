6eace5cc6b91af8cde3a9b2330c2efde7ef1c6a1
Your Name <you@example.com>
2024-09-25 12:46:07 +0300

    index /w orders

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,19 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     public function index(){
-        return Customer::all();
+        // return Customer::all();
+        $results = DB::table('customers as c')
+            ->join('orders as o', 'c.customer_id', '=', 'o.customer_id')
+            ->join('order_statuses as os', 'o.status', '=', 'os.order_status_id')
+            ->select(
+                'c.customer_id',
+                'c.first_name',
+                'c.last_name',
+                'c.address',
+                'c.city',
+                'c.state',
+                'c.points',
+                'o.order_date',
+                'os.name as order_status_name'
+            )
+            ->get();
+
+            return $results;
     }
 
     public function store(Request $request){
         $request->validate([
-            'first_name'=> 'required',
-            'last_name'=> 'required',
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'address'=>'required',
+            'city'=>'required',
+            'state'=>'required',
+            'points'
         ]);
     }
 
@@ -29,7 +51,7 @@ class CustomerController extends Controller
 
     public function update(Request $request, Customer $customer){
 
-    }
+    } 
 
     public function destroy(Customer $customer){
 
